9d69376f26e3d525a84dd8920964b2d78eabbc7a
justinfigueredo <jfiguere@msudenver.edu>
2020-04-14 23:01:55 -0600

    comment

--- app/src/main/java/com/example/org/weather/MainActivity.kt
+++ app/src/main/java/com/example/org/weather/MainActivity.kt
@@ -21,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // this should be in the dev branch
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
