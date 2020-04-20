af2473739a76b238bbb31de665f18b9fac2aa2b8
KenjiMorizono <kenji.morizono1@gmail.com>
2020-04-20 09:31:26 -0600

    Added method stubs for getting the location

--- app/src/main/java/com/example/org/weather/MainActivity.kt
+++ app/src/main/java/com/example/org/weather/MainActivity.kt
@@ -56,6 +56,16 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun getLocation(){ // For when GPS is enabled
+
+
+    }
+
+    fun getEnteredLocation(){ // For getting location based on ZIP code entered in entry field
+
+
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
