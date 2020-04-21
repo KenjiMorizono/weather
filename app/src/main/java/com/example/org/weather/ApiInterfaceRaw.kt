431f6fae3cbc1f9272e8520a119fde568f95c524
justinfigueredo <jfiguere@msudenver.edu>
2020-04-21 13:18:09 -0600

    Able to get the raw json resonse from the weather api

---
+++ app/src/main/java/com/example/org/weather/ApiInterfaceRaw.kt
@@ -0,0 +1,50 @@
+package com.example.org.weather
+
+import android.util.Log
+import com.android.volley.AuthFailureError
+import com.android.volley.Response
+import com.android.volley.VolleyLog
+import com.android.volley.toolbox.StringRequest
+
+class ApiInterfaceRaw {
+    companion object {
+        val TAG = "ApiInterfaceRaw"
+        val apikey = "0rbNpCSqq2OAZ7b16EUoZ3Vv3se1OILI"
+        val climacellURL = "https://api.climacell.co/v3"
+        val realTimeURL = "/weather/realtime?"
+
+        fun GetRealTimeStats(lat: Double, lon: Double, unit: String, fields: List<String>): String? {
+
+            val path = "${realTimeURL}lat=${lat}&lon=${lon}&unit_system=${unit}&fields=${fields.joinToString(",")}"
+
+            var str: String? = null
+            ApiInterfaceRaw.get(path) { response ->
+                str = response
+            }
+
+            return str
+        }
+
+        fun get(path: String, completionHandler: (response: String?) -> Unit) {
+            val stringReq = object : StringRequest(Method.GET, climacellURL + path,
+                Response.Listener<String> { response ->
+                    Log.d(TAG, "/post request OK! Response: $response")
+                    completionHandler(response)
+                },
+                Response.ErrorListener { error ->
+                    VolleyLog.e(TAG, "/post request fail! Error: ${error.message}")
+                    completionHandler(null)
+                }) {
+                @Throws(AuthFailureError::class)
+                override fun getHeaders(): Map<String, String> {
+                    val headers = HashMap<String, String>()
+                    headers.put("accept", "application/json")
+                    headers.put("apikey", apikey)
+                    return headers
+                }
+            }
+
+            BackendVolley.instance?.addToRequestQueue(stringReq, TAG)
+        }
+    }
+}
