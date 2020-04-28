b72ba7a80ce4b014c1fe85a650389cdae48b283f
justinfigueredo <jfiguere@msudenver.edu>
2020-04-28 14:34:37 -0600

    added metric boolean to api interface

--- app/src/main/java/com/example/org/weather/ApiInterface.kt
+++ app/src/main/java/com/example/org/weather/ApiInterface.kt
@@ -7,7 +7,14 @@ class ApiInterface {
     companion object {
         private val gson: Gson = Gson()
 
-        fun GetRealTimeStats(lat: Double, lon: Double, unit: String, completionHandler: (response: RealTimeStats?) -> Unit) {
+        fun GetRealTimeStats(lat: Double, lon: Double, useUSMetric: Boolean, completionHandler: (response: RealTimeStats?) -> Unit) {
+            var unit = if(useUSMetric){
+                "us"
+            }
+            else
+            {
+                "si"
+            }
 
             ApiInterfaceRaw.GetRealTimeStats(lat, lon, unit, arrayListOf("temp", "feels_like", "wind_speed", "visibility", "humidity", "wind_direction", "precipitation", "precipitation_type", "cloud_cover", "fire_index", "sunrise", "sunset", "weather_code")){ response ->
                 var stats = this.gson.fromJson(response, RealTimeStats::class.java)
